d2a998611ae9faefcf41084ec98b6003100ea8d9
Li Ellis Gallardo <lellisga@gmail.com>
2011-05-31 13:58:23 -0500

    change primary key to be objectid

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,9 +1,9 @@
 class Product
   include Mongoid::Document
   field :title, :type => String
-  key :title
   field :description, :type => String
   field :image_url, :type => String
+
   validates_presence_of :title, :description , :image_url
   validates_uniqueness_of :title
 end

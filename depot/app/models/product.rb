84542ac684083da8a2c3729a27ca410604811096
Li Ellis Gallardo <lellisga@gmail.com>
2011-05-30 23:54:54 -0500

    add validations

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -2,8 +2,11 @@ class Product #< ActiveRecord::Base
   include MongoMapper::Document
 
   #attr_accessible :title, :description, :image_url
-
+  
+  key :id, ObjectId
   key :title, String
   key :description, String
   key :image_url, String
+
+  validates_presence_of :title , :description, :image_url
 end
